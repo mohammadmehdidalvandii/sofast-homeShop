07deeddc6591f49a599b1d4029cce58d2ce9ae84
mohammadmehiddalvadnii <mhmdmehdidalvandii@gmail.com>
2024-05-06 16:46:18 +0330

    create register successfuly - validation is not valid auth.js

--- src/app/LoginRegister/page.js
+++ src/app/LoginRegister/page.js
@@ -1,3 +1,4 @@
+"use client";
 import React from 'react'
 import Navbar from '../components/module/Navbar/Navbar'
 import PageHeader from '../components/module/PageHeader/PageHeader'
--- src/app/components/template/loginRegister/Register/Register.js
+++ src/app/components/template/loginRegister/Register/Register.js
@@ -1,8 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './Register.module.css';
+// import { validationEmail ,validationPassword  } from '@/utils/auth';
+import swal from 'sweetalert';
 
 
-function Register() {
+function 
+Register() {
+  const [name , setName] = useState('');
+  const [email , setEmail] = useState('');
+  const [password , setPassword] = useState("");
+  const [passwordVerify , setPasswordVerify] = useState("");
+
+
+  const signUp = async (event)=>{
+    event.preventDefault()
+    // validation input Register
+    if(!name.trim()){
+      return swal({
+        title:"نام خود را واردکنید",
+        icon:"error",
+        buttons:"تلاش مجدد"
+      })
+    } else if(name.length < 4){
+      return swal({
+        title:"نام کاربری حداکثر باید 4حرف باشد",
+        icon:"error",
+        buttons:"تلاش مجدد"
+      })
+    }
+
+    // if(email){
+    //   const isValidEmail = validationEmail(email);
+    //   if(!isValidEmail){
+    //     swal({
+    //       title:"ایمیل معتبر نیست",
+    //       icon:"error",
+    //       buttons:"تلاش مجدد"
+    //     })
+    //   }
+    // }
+
+    // const isValidPassword = validationPassword(password);
+    // if(!isValidPassword){
+    //   swal({
+    //     title:"رمز عبور معتبر نیست",
+    //     icon:"error",
+    //     buttons:"تلاش مجدد"
+    //   })
+    // } else if (password !== passwordVerify){
+    //   swal({
+    //     title:"رمز عبور هماهنگ  نیست",
+    //     icon:"error",
+    //     buttons:"تلاش مجدد"
+    //   })
+    // }
+
+    const user = {name , email , password}
+
+    const res = await fetch('/api/auth/signup',{
+      method: "POST",
+      headers:{
+        "Content-Type" : "application/json",
+      },
+      body : JSON.stringify(user)
+    });
+
+    if(res.status === 201){
+      swal({
+        title:"ثبت نام با موفقیت انجام شد",
+        icon:"success",
+        buttons:"ورود به پنل کاربری"
+      })
+    } else if(res.status === 422){
+      swal({
+        title:"این کاربر از قبل وجود دارد",
+        icon:"error",
+        buttons:"تلاش مجدد"
+      })
+    }
+
+  };
+
+ 
+
   return (
     <section className={style.register}>
                 <div className='content'>
@@ -10,15 +90,35 @@ function Register() {
                 <span className="content_text">اگر مشتری جدید هستید ثبت نام کنید</span>
             </div>
         <form action="#" className="form">
-            <input type="text" className="form_input" placeholder='نام کاربری'/>
-            <input type="text" className="form_input" placeholder='آدرس ایمیل'/>
-            <input type="password" className="form_input" placeholder='رمزعبور'/>
-            <input type="password" className="form_input" placeholder='تکرار رمزعبور'/>
+            <input
+             type="text"
+             value={name}
+             onChange={(event)=> setName(event.target.value)}
+             className="form_input"
+             placeholder='نام کاربری'/>
+            <input
+             type="text" 
+             value={email}
+             onChange={(event)=> setEmail(event.target.value)}
+            className="form_input"
+             placeholder='آدرس ایمیل'/>
+            <input
+             type="password"
+             value={password}
+             onChange={(event)=> setPassword(event.target.value)}
+             className="form_input" 
+            placeholder='رمزعبور'/>
+            <input
+             type="password" 
+             value={passwordVerify}
+             onChange={(event)=> setPasswordVerify(event.target.value)}
+            className="form_input"
+             placeholder='تکرار رمزعبور'/>
             <div className="form_very">
                 <input type='checkBox' className="form_very_input" />
                 <span className="form_very_text">من شرایط و ضوابط را خوانده ام و با آن موافقم</span>
             </div>
-            <button className="form_submit button">ورود</button>
+            <button className="form_submit button" onClick={signUp}>ورود</button>
         </form>
     </section>
   )
